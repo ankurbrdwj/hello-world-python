d48feb7bcbd190514f5f1be336cf8299a0ae1702
ankurbrdwj <ankurbrdwj@gmail.com>
2025-12-07 07:34:08 +0100

    Daily meal planner

--- django-rest/entities/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Entity package initialization
-Django apps should keep this minimal to avoid import issues
-"""
-
-# Keep this file minimal for Django apps
-# Models will be auto-discovered from models.py files
-default_app_config = 'entities.apps.EntitiesConfig'
--- django-rest/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Service package initialization
-"""
-
-from .ingredient_service import IngredientService
-from .recipe_service import RecipeService
-from .meal_schedule_service import MealScheduleService
-
-__all__ = ['IngredientService', 'RecipeService', 'MealScheduleService']
